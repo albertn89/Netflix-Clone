79554546c297f1f448adcbd81d1eac9c20522753
Albert Negoro <albert.negoro89@gmail.com>
2024-02-23 03:18:25 -0700

    Navbar

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -88,7 +88,7 @@ const Auth = () => {
 
 						<div className="flex flex-row items-center gap-4 mt-8 justify-center">
 							<div
-								onClick={() => signIn("google", { callbackUrl: "/" })}
+								onClick={() => signIn("google", { callbackUrl: "/profiles" })}
 								className="
 									w-10
 									h-10
@@ -105,7 +105,7 @@ const Auth = () => {
 								<FcGoogle size={30} />
 							</div>
 							<div
-								onClick={() => signIn("github", { callbackUrl: "/" })}
+								onClick={() => signIn("github", { callbackUrl: "/profiles" })}
 								className="
 									w-10
 									h-10
